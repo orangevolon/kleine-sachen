9b4be460837e51165956e8f09add954f4d7ab789
Amir Alami <abstract.stream@gmail.com>
2023-12-28 23:27:54 +0100

    Add snake grid

--- snake/index.js
+++ snake/index.js
@@ -1,2 +1,23 @@
+function createCells(gridSize) {
+  const cellsCount = gridSize ** 2;
+
+  const cellsContainer = document.createElement("div");
+  cellsContainer.classList.add("cells-container");
+  cellsContainer.style.setProperty("--grid-size", gridSize);
+
+  for (let cellIdx = 0; cellIdx < cellsCount; cellIdx++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.id = `cell-${cellIdx}`;
+
+    cellsContainer.appendChild(cell);
+  }
+
+  return cellsContainer;
+}
+
+const GRID_SIDE = 20;
+
 const app = document.getElementById("app");
-app.innerText = "Snake!";
+const cells = createCells(GRID_SIDE);
+app.appendChild(cells);
