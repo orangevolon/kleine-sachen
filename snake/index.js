ed255d13623182bba96446e1aa4b4bc02949c1e1
Amir Alami <abstract.stream@gmail.com>
2023-12-29 00:14:44 +0100

    Add basic direction

--- snake/index.js
+++ snake/index.js
@@ -1,3 +1,13 @@
+const GRID_SIDE = 20;
+const SPEED_MOVE_PER_SECOND = 2;
+
+const keyDirMap = {
+  ArrowUp: "up",
+  ArrowDown: "down",
+  ArrowLeft: "left",
+  ArrowRight: "right",
+};
+
 function createCells(gridSize) {
   const cellsCount = gridSize ** 2;
 
@@ -16,8 +26,84 @@ function createCells(gridSize) {
   return cellsContainer;
 }
 
-const GRID_SIDE = 20;
+function createSnake(gridSize, initialDirection = "right") {
+  let direction = initialDirection;
+  const initialHeadIdx = Math.floor(gridSize ** 2 / 2);
+  const positions = [initialHeadIdx];
 
-const app = document.getElementById("app");
-const cells = createCells(GRID_SIDE);
-app.appendChild(cells);
+  function getNext(head, direction) {
+    switch (direction) {
+      case "right":
+        return head + 1;
+      case "left":
+        return head - 1;
+      case "up":
+        return head - gridSize;
+      case "down":
+        return head + gridSize;
+    }
+  }
+
+  function setDirection(newDirection) {
+    direction = newDirection;
+  }
+
+  function progress() {
+    const head = positions[positions.length - 1];
+    const added = getNext(head, direction);
+    positions.push(added);
+
+    const removed = positions.shift();
+
+    return {
+      added: [added],
+      removed: [removed],
+    };
+  }
+
+  return {
+    setDirection,
+    progress,
+    positions,
+  };
+}
+
+function renderSnake(diff, cellContainer) {
+  for (const position of diff.added) {
+    const cell = cellContainer.querySelector(`#cell-${position}`);
+    cell.classList.add("snake-cell");
+  }
+
+  for (const position of diff.removed) {
+    const cell = cellContainer.querySelector(`#cell-${position}`);
+    cell.classList.remove("snake-cell");
+  }
+}
+
+function prepareScene() {
+  const app = document.getElementById("app");
+  const cells = createCells(GRID_SIDE);
+  app.appendChild(cells);
+
+  const snake = createSnake(GRID_SIDE);
+  document.addEventListener("keydown", (event) => {
+    const direction = keyDirMap[event.key];
+    if (direction) snake.setDirection(direction);
+  });
+
+  return { snake, cells };
+}
+
+function runGame() {
+  const { snake, cells } = prepareScene();
+
+  const interval = 1000 / SPEED_MOVE_PER_SECOND;
+  setInterval(() => {
+    const diff = snake.progress();
+    requestAnimationFrame(() => {
+      renderSnake(diff, cells);
+    });
+  }, interval);
+}
+
+runGame();
