12796187817c032af650f3c1de19f128a1286a63
Amir Alami <abstract.stream@gmail.com>
2024-01-01 13:20:35 +0100

    Add wall guardrails

--- snake/index.js
+++ snake/index.js
@@ -26,21 +26,35 @@ function createCells(gridSize) {
   return cellsContainer;
 }
 
-function createSnake(gridSize, initialDirection = "right") {
+function createSnake({
+  gridSize,
+  speed,
+  initialDirection = "right",
+  onProgress,
+  onFail,
+}) {
   let direction = initialDirection;
   const initialHeadIdx = Math.floor(gridSize ** 2 / 2);
   const positions = [initialHeadIdx];
 
   function getNext(head, direction) {
     switch (direction) {
-      case "right":
+      case "right": {
+        if (head % gridSize === gridSize - 1) throw head;
         return head + 1;
-      case "left":
+      }
+      case "left": {
+        if (head % gridSize === 0) throw head;
         return head - 1;
-      case "up":
+      }
+      case "up": {
+        if (head < gridSize) throw head;
         return head - gridSize;
-      case "down":
+      }
+      case "down": {
+        if (head > (gridSize - 1) * gridSize) throw head;
         return head + gridSize;
+      }
     }
   }
 
@@ -50,16 +64,33 @@ function createSnake(gridSize, initialDirection = "right") {
 
   function progress() {
     const head = positions[positions.length - 1];
-    const added = getNext(head, direction);
-    positions.push(added);
 
-    const removed = positions.shift();
+    try {
+      const added = getNext(head, direction);
+      positions.push(added);
+      const removed = positions.shift();
 
-    return {
-      added: [added],
-      removed: [removed],
-    };
+      return {
+        added: [added],
+        removed: [removed],
+      };
+    } catch (hit) {
+      return { hit };
+    }
   }
+
+  // run the snake
+  const intervalTimeMs = 1000 / speed;
+  const interval = setInterval(() => {
+    const result = progress();
+
+    if (result.hit) {
+      clearInterval(interval);
+      onFail?.(result);
+    } else {
+      onProgress(result);
+    }
+  }, intervalTimeMs);
 
   return {
     setDirection,
@@ -68,42 +99,44 @@ function createSnake(gridSize, initialDirection = "right") {
   };
 }
 
-function renderSnake(diff, cellContainer) {
-  for (const position of diff.added) {
-    const cell = cellContainer.querySelector(`#cell-${position}`);
-    cell.classList.add("snake-cell");
-  }
+function createProgressHandler(cellContainer) {
+  return function handleProgress(progressDiff) {
+    for (const position of progressDiff.added) {
+      const cell = cellContainer.querySelector(`#cell-${position}`);
+      cell.classList.add("snake-cell");
+    }
 
-  for (const position of diff.removed) {
-    const cell = cellContainer.querySelector(`#cell-${position}`);
-    cell.classList.remove("snake-cell");
-  }
+    for (const position of progressDiff.removed) {
+      const cell = cellContainer.querySelector(`#cell-${position}`);
+      cell.classList.remove("snake-cell");
+    }
+  };
 }
 
-function prepareScene() {
+function createFailHandler() {
+  return function failHandler({ hit }) {
+    console.log("hit the wall!", hit);
+  };
+}
+
+function runGame() {
   const app = document.getElementById("app");
   const cells = createCells(GRID_SIDE);
   app.appendChild(cells);
 
-  const snake = createSnake(GRID_SIDE);
+  const snake = createSnake({
+    gridSize: GRID_SIDE,
+    speed: SPEED_MOVE_PER_SECOND,
+    onProgress: createProgressHandler(cells),
+    onFail: createFailHandler(),
+  });
+
   document.addEventListener("keydown", (event) => {
     const direction = keyDirMap[event.key];
     if (direction) snake.setDirection(direction);
   });
 
   return { snake, cells };
-}
-
-function runGame() {
-  const { snake, cells } = prepareScene();
-
-  const interval = 1000 / SPEED_MOVE_PER_SECOND;
-  setInterval(() => {
-    const diff = snake.progress();
-    requestAnimationFrame(() => {
-      renderSnake(diff, cells);
-    });
-  }, interval);
 }
 
 runGame();
