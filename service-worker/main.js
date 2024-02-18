3a6cabd327fa30fc414b9a4239ddd3ff736d4a1f
Amir Alami <abstract.stream@gmail.com>
2024-02-18 10:40:47 +0100

    Add images

--- service-worker/main.js
+++ service-worker/main.js
@@ -8,4 +8,39 @@ async function initSw() {
   }
 }
 
-initSw();
+async function createImages(root) {
+  const baseUrl = "https://picsum.photos/";
+  const size = 200;
+  const count = 64;
+
+  function createImageContainer() {
+    const container = document.createElement("section");
+    container.classList.add("image-container");
+
+    root.appendChild(container);
+    return container;
+  }
+
+  function createImage(container, id) {
+    const img = document.createElement("img");
+    const { origin } = new URL(baseUrl);
+    img.src = `${origin}/${size}?random=${id}`;
+    img.classList.add("image");
+
+    container.appendChild(img);
+  }
+
+  function createImages(container) {
+    for (let idx = 0; idx < count; idx++) createImage(container, idx);
+  }
+
+  const container = createImageContainer();
+  createImages(container);
+}
+
+(async function main() {
+  await initSw();
+
+  const root = document.getElementById("root");
+  createImages(root);
+})();
